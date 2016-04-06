4ef637cc31801007d5e2debda7ba76d660a2c461
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-06 14:26:59 -0400

    Changed the seed files to make smaller database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 ActiveRecord::Base.transaction do
-  50.times do |i|
-    
+  Product.all.each { |u| u.destroy! }
+  User.all.each { |u| u.destroy! }
+
+  make_products = Proc.new do |user|
+    8.times do |i|
+      product = FactoryGirl.create :product, user: user
+    end
   end
+
+  steve = User.create! id: 1, email: 'zelaznik@yahoo.com', password: 'password'
+  make_products.call(steve)
+  (2...8).each do |i|
+    user = FactoryGirl.create :user
+    make_products.call(user)
+  end
+
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :product do
     title { Faker::Product.product_name }
     price { rand() * 100 }
-    published { false }
+    published { (rand()*2 <= 1) }
     user { FactoryGirl.create :user }
   end
 end
