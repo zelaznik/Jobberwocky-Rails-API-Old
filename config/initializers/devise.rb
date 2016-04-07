7eb8cb0484d09ffa582e7eeebf9714dd41e09ceb
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-07 18:00:03 -0400

    Pulled github secrets from env variables.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,10 +1,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :github,
-                  Rails.application.secrets.github_client_id,
-                  Rails.application.secrets.github_client_secret,
-                  scope: 'user:email'
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
