90c5c6dc7783d1e3d34d1f42e07428530b0403d4
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-06 22:35:42 -0400

    Removed api subdomain as a constraint.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/api_constraints.rb'
 
 Jobberwocky::Application.routes.draw do
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/"  do
+  namespace :api, defaults: { format: :json }, path: "/"  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       match '*/*' => 'cors#preflight', :via => :options
 
