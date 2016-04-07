09343650dfb368521fd750b9cb1c93a4125fcfe1
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-07 16:38:10 -0400

    Added new fields to user model for omni auth.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407121551) do
+ActiveRecord::Schema.define(version: 20160407194937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20160407121551) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "auth_token",             default: ""
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
