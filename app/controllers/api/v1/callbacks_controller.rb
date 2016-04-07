029d8e34a663aa52659241a03192abff4ef2bc8e
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-07 18:03:03 -0400

    Added callbacks controller.

--- app/controllers/api/v1/callbacks_controller.rb
+++ app/controllers/api/v1/callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CallbacksController < Devise::OmniauthCallbacksController
   def github
+    debugger
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
   end
