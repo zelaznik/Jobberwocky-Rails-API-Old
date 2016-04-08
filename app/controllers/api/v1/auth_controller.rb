4b5a4b0bd3ca3fb065c6d4f6b0bd559ac85732d8
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-08 00:33:00 -0400

    Separated secruity risky data into a secret json file, rather than YML.

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,17 +1,20 @@
 class Api::V1::AuthController < ApplicationController
+  include SecretsHelper
   def github
     settings = {
         url: 'https://github.com/login/oauth/authorize',
         type: 'GET',
         query: {
             scope: 'user',
-            client_id: '16f96b58b2b320779fdb',
-            string: "#{Devise.friendly_token}",
+            client_id: secrets_json["GITHUB_CLIENT_ID"],
+            state: Devise.friendly_token,
             redirect_uri: 'http://api.jobberwocky.dev/auth/callback'
         }
     }
-    query_pairs = URI.encode_www_form(settings[:query].to_a)
-    full_url = "#{settings[:url]}?#{query_pairs}"
+
+    key_val_pairs = settings[:query].to_a
+    query_string = URI.encode_www_form(key_val_pairs)
+    full_url = "#{settings[:url]}?#{query_string}"
     render json: {settings: settings, url: full_url}, status: 200
   end
 
