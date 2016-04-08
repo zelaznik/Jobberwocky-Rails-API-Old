e89b365aa5fc2ed23f25ab033149040d8d67ce03
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-08 00:34:03 -0400

    Moved onmiauth values into a secrets.json file too.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user:email'
+  secrets_path = Rails.root.join('config','secrets.json');
+  env = File.open(secrets_path, 'r') {|f| JSON.parse(f.read)[Rails.env] }
+
+  provider :github, env["GITHUB_CLIENT_ID"], env["GITHUB_CLIENT_SECRET"], scope: 'user:email'
 end
