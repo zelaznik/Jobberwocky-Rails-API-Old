92c21ff4505e70cb69d2d8c27f34af47c616b9cd
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-08 23:19:26 -0400

    From 47 down to 43

--- spec/controllers/static/companies_controller_spec.rb
+++ spec/controllers/static/companies_controller_spec.rb
@@ -31,33 +31,33 @@ describe Static::CompaniesController, fornat: :html do
   let(:valid_session) { {} }
 
   describe "GET index" do
-    it "assigns all static_companies as @companies" do
+    it "assigns all companies as @companies" do
       company = Company.create! valid_attributes
       get :index, {}, valid_session
-      assigns(:static_companies).should eq([company])
+      assigns(:companies).should eq([company])
     end
   end
 
   describe "GET show" do
-    it "assigns the requested static_company as @company" do
+    it "assigns the requested company as @company" do
       company = Company.create! valid_attributes
       get :show, {:id => company.to_param}, valid_session
-      assigns(:static_company).should eq(company)
+      assigns(:company).should eq(company)
     end
   end
 
   describe "GET new" do
-    it "assigns a new static_company as @company" do
+    it "assigns a new company as @company" do
       get :new, {}, valid_session
-      assigns(:static_company).should be_a_new(Company)
+      assigns(:company).should be_a_new(Company)
     end
   end
 
   describe "GET edit" do
-    it "assigns the requested static_company as @company" do
+    it "assigns the requested company as @company" do
       company = Company.create! valid_attributes
       get :edit, {:id => company.to_param}, valid_session
-      assigns(:static_company).should eq(company)
+      assigns(:company).should eq(company)
     end
   end
 
@@ -65,34 +65,34 @@ describe Static::CompaniesController, fornat: :html do
     describe "with valid params" do
       it "creates a new Company" do
         expect {
-          post :create, {:static_company => valid_attributes}, valid_session
+          post :create, {:company => valid_attributes}, valid_session
         }.to change(Company, :count).by(1)
       end
 
-      it "assigns a newly created static_company as @company" do
-        post :create, {:static_company => valid_attributes}, valid_session
-        assigns(:static_company).should be_a(Company)
-        assigns(:static_company).should be_persisted
+      it "assigns a newly created company as @company" do
+        post :create, {:company => valid_attributes}, valid_session
+        assigns(:company).should be_a(Company)
+        assigns(:company).should be_persisted
       end
 
-      it "redirects to the created static_company" do
-        post :create, {:static_company => valid_attributes}, valid_session
+      it "redirects to the created company" do
+        post :create, {:company => valid_attributes}, valid_session
         response.should redirect_to(Company.last)
       end
     end
 
     describe "with invalid params" do
-      it "assigns a newly created but unsaved static_company as @company" do
+      it "assigns a newly created but unsaved company as @company" do
         # Trigger the behavior that occurs when invalid params are submitted
         Company.any_instance.stub(:save).and_return(false)
-        post :create, {:static_company => { "name" => "invalid value" }}, valid_session
-        assigns(:static_company).should be_a_new(Company)
+        post :create, {:company => { "name" => "invalid value" }}, valid_session
+        assigns(:company).should be_a_new(Company)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Company.any_instance.stub(:save).and_return(false)
-        post :create, {:static_company => { "name" => "invalid value" }}, valid_session
+        post :create, {:company => { "name" => "invalid value" }}, valid_session
         response.should render_template("new")
       end
     end
@@ -100,60 +100,60 @@ describe Static::CompaniesController, fornat: :html do
 
   describe "PUT update" do
     describe "with valid params" do
-      it "updates the requested static_company" do
+      it "updates the requested company" do
         company = Company.create! valid_attributes
-        # Assuming there are no other static_companies in the database, this
+        # Assuming there are no other companies in the database, this
         # specifies that the Company created on the previous line
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
         Company.any_instance.should_receive(:update).with({ "name" => "MyString" })
-        put :update, {:id => company.to_param, :static_company => { "name" => "MyString" }}, valid_session
+        put :update, {:id => company.to_param, :company => { "name" => "MyString" }}, valid_session
       end
 
-      it "assigns the requested static_company as @company" do
+      it "assigns the requested company as @company" do
         company = Company.create! valid_attributes
-        put :update, {:id => company.to_param, :static_company => valid_attributes}, valid_session
-        assigns(:static_company).should eq(company)
+        put :update, {:id => company.to_param, :company => valid_attributes}, valid_session
+        assigns(:company).should eq(company)
       end
 
-      it "redirects to the static_company" do
+      it "redirects to the company" do
         company = Company.create! valid_attributes
-        put :update, {:id => company.to_param, :static_company => valid_attributes}, valid_session
+        put :update, {:id => company.to_param, :company => valid_attributes}, valid_session
         response.should redirect_to(company)
       end
     end
 
     describe "with invalid params" do
-      it "assigns the static_company as @company" do
+      it "assigns the company as @company" do
         company = Company.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Company.any_instance.stub(:save).and_return(false)
-        put :update, {:id => company.to_param, :static_company => { "name" => "invalid value" }}, valid_session
-        assigns(:static_company).should eq(company)
+        put :update, {:id => company.to_param, :company => { "name" => "invalid value" }}, valid_session
+        assigns(:company).should eq(company)
       end
 
       it "re-renders the 'edit' template" do
         company = Company.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Company.any_instance.stub(:save).and_return(false)
-        put :update, {:id => company.to_param, :static_company => { "name" => "invalid value" }}, valid_session
+        put :update, {:id => company.to_param, :company => { "name" => "invalid value" }}, valid_session
         response.should render_template("edit")
       end
     end
   end
 
   describe "DELETE destroy" do
-    it "destroys the requested static_company" do
+    it "destroys the requested company" do
       company = Company.create! valid_attributes
       expect {
         delete :destroy, {:id => company.to_param}, valid_session
       }.to change(Company, :count).by(-1)
     end
 
-    it "redirects to the static_companies list" do
+    it "redirects to the companies list" do
       company = Company.create! valid_attributes
       delete :destroy, {:id => company.to_param}, valid_session
-      response.should redirect_to(static_companies_url)
+      response.should redirect_to(companies_url)
     end
   end
 
