ce94905c27a6eea33486f1f568280e3ca6f3e26d
Steve Zelaznik <zelaznik@yahoo.com>
2016-04-10 17:26:57 -0400

    Decrements the product quantity by the placement quantity

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -17,6 +17,11 @@ describe Api::V1::OrdersController do
       expect(order_response[:id]).to be_present
     end
 
+    it "embeds the two product objects related to the order" do
+      order_response = json_response[:order]
+      expect(order_response[:products].size).to eql 2
+    end
+
     it { should respond_with 201 }
   end
 
